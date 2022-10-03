713e995114193493c772e1fdacf3012bb06f815d
abdelmageed <midohamdi7421@gmail.com>
2022-10-03 18:43:50 +0200

    change MealItemForm to add item in cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,21 @@
-import React from "react";
+import { useContext } from "react";
 
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
   const price = `$${props.price.toFixed(2)}`;
   return (
     <i className={classes.meal}>
@@ -12,7 +24,7 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <MealItemForm id={props.id} />
+      <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
     </i>
   );
 };
